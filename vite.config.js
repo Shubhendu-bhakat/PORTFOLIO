db42d05b7a5982fdbcbff4caeb378992f098e0c9
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2025-03-28 02:19:06 +0530

    modified the viteconfig.js file for AWS

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    modules:{
-      localsConvention:"camelCase",
+  css: {
+    modules: {
+      localsConvention: "camelCase",
     },
   },
+  server: {
+    host: '0.0.0.0', // This allows the server to listen on all network interfaces
+    port: 5173,       // Optional: Ensure Vite is listening on port 5173
+  }
 })
